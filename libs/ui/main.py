a32d583a6601cd20cd9d761710e06a5551c3c61c
meatbag93 <cccefg2@gmail.com>
2022-09-17 02:29:47 +0300

    feat: Add a close button in the temp interface.

--- libs/ui/main.py
+++ libs/ui/main.py
@@ -1,15 +1,35 @@
 import wx
+from ..utils import labeled_widget
+
+
 class Main(wx.Frame):
-    def __init__(self, title: str = "Reddy", client_id: str = "", client_secret: str = "", user_agent: str = "reddy", db_path: str = ""):
-        super().__init__(None, title = title)
+    def __init__(
+        self,
+        title: str = "Reddy",
+        client_id: str = "",
+        client_secret: str = "",
+        user_agent: str = "reddy",
+        db_path: str = "",
+    ):
+        super().__init__(None, title=title)
         self.title: str = title
         self.client_id: str = client_id
         self.client_secret: str = client_secret
         self.user_agent: str = user_agent
         self.db_path: str = db_path
-        self.pannel: wx.Panel = wx.Panel(self)
+        self.panel: wx.Panel = wx.Panel(self)
         self.main_sizer: wx.BoxSizer = wx.BoxSizer()
-        self.temp: wx.TextCtrl = wx.TextCtrl(self.pannel, style = wx.TE_DONTWRAP|wx.TE_READONLY)
-        self.temp.SetValue("This is a test. You cannot do anything at the moment.")
-        self.main_sizer.Add(self.temp, wx.EXPAND|wx.ALIGN_CENTER)
-        self.pannel.SetSizer(self.main_sizer)
+        self.temp_label, self.temp, self.temp_sizer = labeled_widget(
+            self.panel,
+            "Come back later",
+            wx.TextCtrl(
+                self.panel,
+                value="Nothing to do now, come back later when a feature or 2 is implemented!",
+                style = wx.TE_READONLY|wx.TE_MULTILINE|wx.TE_DONTWRAP
+            ),
+        )
+        self.main_sizer.Add(self.temp_sizer, flag=wx.EXPAND)
+        self.temp_button = wx.Button(self.panel, label = "Close")
+        self.temp_button.Bind(wx.EVT_BUTTON, lambda event: self.Close())
+        self.main_sizer.Add(self.temp_button)
+        self.panel.SetSizer(self.main_sizer)

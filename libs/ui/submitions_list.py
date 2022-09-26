5c4d8594e4f3b38907185ea07b01117113bcbc8f
meatbag93 <cccefg2@gmail.com>
2022-09-26 15:06:01 +0300

    feat: Add a column for link flairs

--- libs/ui/submitions_list.py
+++ libs/ui/submitions_list.py
@@ -20,7 +20,7 @@ class SubmitionsList(Gtk.Box):
         self.connect("destroy", lambda _: setattr(self, "_active", False))
         self.label = Gtk.Label(label)
         self.add(self.label)
-        self.liststore = Gtk.ListStore(str, str, str, int, int, bool, bool, object)
+        self.liststore = Gtk.ListStore(str, str, str, str, int, int, bool, bool, object)
         self.view = Gtk.TreeView()
         self.view.connect("row-activated", self.on_activate)
         self.view.set_model(self.liststore)
@@ -39,20 +39,22 @@ class SubmitionsList(Gtk.Box):
             "Subreddit", self.text_renderer, text=0
         )
         self.view.append_column(self.subreddit_name_column)
-        self.title_column = Gtk.TreeViewColumn("Title", self.text_renderer, text=1)
+        self.link_flair_column = Gtk.TreeViewColumn("Link flair", self.text_renderer, text=1)
+        self.view.append_column(self.link_flair_column)
+        self.title_column = Gtk.TreeViewColumn("Title", self.text_renderer, text=2)
         self.view.append_column(self.title_column)
-        self.author_column = Gtk.TreeViewColumn("Author", self.text_renderer, text=2)
+        self.author_column = Gtk.TreeViewColumn("Author", self.text_renderer, text=3)
         self.view.append_column(self.author_column)
-        self.upvotes_column = Gtk.TreeViewColumn("Upvotes", self.text_renderer, text=3)
+        self.upvotes_column = Gtk.TreeViewColumn("Upvotes", self.text_renderer, text=4)
         self.view.append_column(self.upvotes_column)
         self.comments_column = Gtk.TreeViewColumn(
-            "Comments", self.text_renderer, text=4
+            "Comments", self.text_renderer, text=5
         )
         self.view.append_column(self.comments_column)
-        self.upvote_column = Gtk.TreeViewColumn("Upvote", self.upvote_toggle, active=5)
+        self.upvote_column = Gtk.TreeViewColumn("Upvote", self.upvote_toggle, active=6)
         self.view.append_column(self.upvote_column)
         self.downvote_column = Gtk.TreeViewColumn(
-            "Downvote", self.downvote_toggle, active=6
+            "Downvote", self.downvote_toggle, active=7
         )
         self.view.append_column(self.downvote_column)
         self.append_more_submissions()
@@ -76,6 +78,7 @@ class SubmitionsList(Gtk.Box):
             item.append("")
         item.extend(
             [
+                submission.link_flair_text or "",
                 submission.title,
                 submission.author.name,
                 submission.score,
@@ -99,33 +102,33 @@ class SubmitionsList(Gtk.Box):
 
     def do_upvote(self, renderer, treepath):
         row = self.liststore[treepath]
-        upvote_value = row[5]
-        submission = row[7]
+        upvote_value = row[6]
+        submission = row[8]
         if not upvote_value:
             threadpool.submit(submission.upvote, None, threadpool=1)
-            row[3] += 1
+            row[4] += 1
         else:
             threadpool.submit(submission.clear_vote, None, threadpool=1)
-            row[3] -= 1
-        row[5] = not upvote_value
-        row[6] = False
+            row[4] -= 1
+        row[6] = not upvote_value
+        row[7] = False
 
     def do_downvote(self, renderer, treepath):
         row = self.liststore[treepath]
-        downvote_value = row[6]
-        submission = row[7]
+        downvote_value = row[7]
+        submission = row[8]
         if not downvote_value:
             threadpool.submit(submission.downvote, None, threadpool=1)
-            row[3] -= 1
+            row[4] -= 1
         else:
             threadpool.submit(submission.clear_vote, None, threadpool=1)
-            row[3] += 1
-        row[6] = not downvote_value
-        row[5] = False
+            row[4] += 1
+        row[7] = not downvote_value
+        row[6] = False
 
     def on_activate(self, widget, row, column):
         iter_ = self.liststore.get_iter(row)
-        submission = self.liststore.get_value(iter_, 7)
+        submission = self.liststore.get_value(iter_, 8)
         dialog = submission_dialog.SubmissionDialog(self.get_toplevel(), submission)
         dialog.show_all()
         dialog.run()
